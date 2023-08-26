044dd8509c21ac660c8e21e55843e3e757274e31
HeadMad <kov.ps@ya.ru>
2023-08-26 16:41:10 +0800

    set node-fetch

--- api/[same].js
+++
@@ -1,8 +0,0 @@
-// import createFetch from '@vercel/fetch';
-import axios from 'axios';
-// const fetch = createFetch(axios);
-
-export default async function handler(request, response) {
-  const {data} = await axios('https://jsonplaceholder.typicode.com/todos/1');
-  response.status(200).json(data);
-}
--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
+import fetch from 'node-fetch';
 
-export default async function handler(request, response) {
-  
-  response.status(200).json({hello: 'me'});
+export default async function handler(req, res) {
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(res => res.json());
+  console.log(data)
+  res.status(200).json(data);
 }
