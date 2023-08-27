0a684f35cb1ce17da04de0eaf0d2a154e684b407
Kovalenko Pavel <kov.ps@ya.ru>
2023-08-28 03:07:01 +0800

    testing bot

--- api/index.js
+++ api/index.js
@@ -1,20 +1,7 @@
 import bot from '../src/bot.js';
 
 export default async function handler(req, res) {
-  // if (!req.body)
-  //   return res.status(400).json({ error: 'No request body' });
-// res.status(200).send();
-  // bot.getMe().then(result => {
-  //   res.status(200).send(result);
-  // });
-
-  // console.log(JSON.stringify(req.body));
-  // console.log('debug 1');
-// console.log(process.env.TOKEN)
-//   bot.sendMessage({
-//     chat_id: 1271693775,
-//     text: 'hhhhello',
-//   })
+  console.log(req.body);
   
   bot.process(req.body);
   res.status(200).send('');
--- src/bot.js
+++ src/bot.js
@@ -3,9 +3,8 @@ import Telegrambo from "../lib/telegrambo/index.js";
 const bot = new Telegrambo(process.env.TOKEN);
 
 bot.on("message", (ctx) => {
-  console.log('run handler');
   ctx.sendMessage({
-    text: 'mess'
+    text: ctx.message.text
   })
   
 });
