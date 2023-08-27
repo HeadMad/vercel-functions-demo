3c43dd414e3619b821e9c0695bad3a3d9906afb1
Kovalenko Pavel <kov.ps@ya.ru>
2023-08-28 05:21:41 +0800

    fixess

--- api/index.js
+++ api/index.js
@@ -3,28 +3,28 @@ import bot from '../src/bot.js';
 export default async function handler(req, res) {
   // console.log(req.body);
   
-  const results = bot.process({
-    "update_id": 83019536,
-    "message": {
-     "message_id": 1863,
-     "from": {
-      "id": 1271693775,
-      "is_bot": false,
-      "first_name": "Павел",
-      "username": "kovapaul",
-      "language_code": "ru"
-     },
-     "chat": {
-      "id": 1271693775,
-      "first_name": "Павел",
-      "username": "kovapaul",
-      "type": "private"
-     },
-     "date": 1692690612,
-     "text": "thyuhe"
-    }
-   })
-    res.status(200).json(results[0]);
-  // bot.process(req.body);
+  // const results = bot.process({
+  //   "update_id": 83019536,
+  //   "message": {
+  //    "message_id": 1863,
+  //    "from": {
+  //     "id": 1271693775,
+  //     "is_bot": false,
+  //     "first_name": "Павел",
+  //     "username": "kovapaul",
+  //     "language_code": "ru"
+  //    },
+  //    "chat": {
+  //     "id": 1271693775,
+  //     "first_name": "Павел",
+  //     "username": "kovapaul",
+  //     "type": "private"
+  //    },
+  //    "date": 1692690612,
+  //    "text": "thyuhe"
+  //   }
+  //  })
+  const results = bot.process(req.body);
+  res.status(200).json(results[0]);
   
 }
