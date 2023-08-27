251fef19da43c8b208507e5bf04d6471841e5cbe
Kovalenko Pavel <kov.ps@ya.ru>
2023-08-28 01:36:58 +0800

    Fix index

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import bot from '../src/bot.js';
 
 export default async function handler(req, res) {
-  if (!req.body)
-    return res.status(400).json({ error: 'No request body' });
+  // if (!req.body)
+  //   return res.status(400).json({ error: 'No request body' });
+// res.status(200).send();
+  // bot.getMe().then(result => {
+  //   res.status(200).send(result);
+  // });
 
   console.log(JSON.stringify(req.body));
   bot.process(req.body);
-  res.status(200);
+  res.status(200).send('');
 }
