9ce9f579ed5dceca1461a29b059f6deccb200d8f
Kovalenko Pavel <kov.ps@ya.ru>
2023-08-28 05:17:23 +0800

    hot bot fix

--- src/bot.js
+++ src/bot.js
@@ -3,14 +3,10 @@ import Telegrambo from "../lib/telegrambo/index.js";
 const bot = new Telegrambo(process.env.TOKEN);
 
 bot.on("message", (ctx) => {
-  console.log(ctx.message)
-  // console.log('=============')
-  ctx.sendMessage({
-    text: ctx.message.text
-  })
-  .then(console.log)
-  .catch(console.log)
-  
+  return {
+    ...ctx.payload,
+    text: Date.now()
+  }
 });
 
 export default bot;
