6d7af72cea371f54b8a406feff6a6a144715fa80
Kovalenko Pavel <kov.ps@ya.ru>
2023-08-28 02:35:28 +0800

    botfixes 4

--- api/index.js
+++ api/index.js
@@ -9,6 +9,27 @@ export default async function handler(req, res) {
   // });
 
   // console.log(JSON.stringify(req.body));
-  bot.process(req.body);
+  console.log('debug 1');
+  // bot.process({
+  //   "update_id": 83019536,
+  //   "message": {
+  //    "message_id": 1863,
+  //    "from": {
+  //     "id": 1271693775,
+  //     "is_bot": false,
+  //     "first_name": "Павел",
+  //     "username": "kovapaul",
+  //     "language_code": "ru"
+  //    },
+  //    "chat": {
+  //     "id": 1271693775,
+  //     "first_name": "Павел",
+  //     "username": "kovapaul",
+  //     "type": "private"
+  //    },
+  //    "date": 1692690612,
+  //    "text": "thyuhe"
+  //   }
+  //  });
   res.status(200).send('');
 }
