796c6cb2029ff6369a4d60d19248efe4a5ef0992
Kovalenko Pavel <kov.ps@ya.ru>
2023-08-28 03:21:32 +0800

    botfixess

--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,9 @@ import Telegrambo from "../lib/telegrambo/index.js";
 const bot = new Telegrambo(process.env.TOKEN);
 
 bot.on("message", (ctx) => {
+  console.log(ctx.update)
   ctx.sendMessage({
-    text: JSON.stringify(ctx.update)
+    text: ctx.message.text
   })
   
 });
