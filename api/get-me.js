a6529144f34504200fc4946acefdaf03bbbe4151
HeadMad <kov.ps@ya.ru>
2023-09-04 13:46:26 +0800

    add actions

--- api/get-me.js
+++ api/get-me.js
@@ -1,9 +1,6 @@
 import bot from '../src/bot.js';
 
 export default function(req, res) {
-  const result = bot.sendMessage({
-    chat_id: 1271693775,
-    text: 'hi there!'
-  });
+  const result = bot.getMe();
   res.status(200).json(result);
 };
--- api/get-webhook-info.js
+++ api/get-webhook-info.js
@@ -2,6 +2,6 @@ import bot from '../src/bot.js';
 
 export default function (req, res) {
   // res.status(200).json({hello:process.env.TOKEN})
-  const result = bot.setWebhook({url: process.env.WEBHOOK_URL});
-  res.status(200).json(result);
+  const result = bot.getWebhookInfo();
+  res.status(200).send(`<pre>${JSON.stringify(result, null, ' ')}</pre>`);
 }
