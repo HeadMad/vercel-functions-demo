0e4b5f00715b89d066fa904f5a84945ad386ec0b
HeadMad <kov.ps@ya.ru>
2023-09-13 18:02:15 +0800

    check callback sendMessage

--- api/index.js
+++ api/index.js
@@ -6,12 +6,15 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
 
     bot.on("message", (ctx) => {
-      res.status(200).json({...ctx.payload, method: 'sendMessage', text: ctx.message.text});
+      return {...ctx.payload, method: 'sendMessage', text: ctx.message.text};
     });
 
     const results = bot.process(req.body);
-    // console.log(results);
+    console.log(results);
+    res.status(200).json(results[0]);
+
   } else 
 
   res.status(200).send('ok');
+
 }
