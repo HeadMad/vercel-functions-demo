87bbbd5ced4128d53290362aae77566aa18a20fb
HeadMad <kov.ps@ya.ru>
2023-09-13 17:56:15 +0800

    check callback answer

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
-import bot from '../src/bot.js';
+// import bot from '../src/bot.js';
+
+const bot = new Telegrambo(process.env.TOKEN);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+
+    bot.on("message", (ctx) => {
+      res.status(200).json({...ctx.payload, method: 'sendMessage', text: ctx.message.text});
+    });
+
     const results = bot.process(req.body);
-    console.log(results);
+    // console.log(results);
   }
 
-  res.status(200).send('ok');
+  // res.status(200).send('ok');
 }
