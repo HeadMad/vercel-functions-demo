4ea1f76514f472e708c11de2643a87b1ce7899d0
Kovalenko Pavel <kov.ps@ya.ru>
2023-09-14 14:30:01 +0800

    fix req.body

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import bot from '../src/bot.js';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const results = bot.process(update);
+    const results = bot.process(req.body);
     console.log(results);
 
     res.status(200).json(results[0]);
