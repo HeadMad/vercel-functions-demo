1a094c85ffefef08b50292509b72f9714e59ae0e
Kovalenko Pavel <kov.ps@ya.ru>
2023-10-10 00:49:47 +0800

    add /wifi

--- src/bot.js
+++ src/bot.js
@@ -7,11 +7,19 @@ bot.on('message::text::hello', ({result}, event, match) => {
 });
 
 bot.on("message", (ctx) => {
+  if (ctx.message.text === '/wifi')
+  return ctx.sendMessage({
+    text: '<a href="WIFI:S:SFhotel;T:WPA;P:alfa2404;H:false;;">Подключиться к WiFi</a>',
+    parse_mode: 'HTML'
+  });
+  
   return ctx.sendMessage({
     text: `<pre>${JSON.stringify(ctx.update, null, ' ')}</pre>`,
     parse_mode: 'HTML',
   })
 });
+
+
 
 
 
