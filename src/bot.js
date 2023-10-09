5b188b3e2b3eb7bf91aa6d238924152d5b50bf21
Kovalenko Pavel <kov.ps@ya.ru>
2023-10-10 01:02:07 +0800

    fix /wifi

--- src/bot.js
+++ src/bot.js
@@ -9,10 +9,15 @@ bot.on('message::text::hello', ({result}, event, match) => {
 bot.on("message", (ctx) => {
   if (ctx.message.text === '/wifi')
   return ctx.sendMessage({
-    text: '<a href="WIFI:S:SFhotel;T:WPA;P:alfa2404;H:false;;">Подключиться к WiFi</a>',
-    parse_mode: 'HTML'
+    text: 'Нажимай и подключайся к WiFi',
+    parse_mode: 'HTML',
+    reply_markup: {
+      inline_keyboard: [
+        [{text: 'Подключиться к WiFi', url: 'WIFI:S:SFhotel;T:WPA;P:alfa2404;H:false;'}]
+      ]
+    }
   });
-  
+
   return ctx.sendMessage({
     text: `<pre>${JSON.stringify(ctx.update, null, ' ')}</pre>`,
     parse_mode: 'HTML',
